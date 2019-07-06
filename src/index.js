cf6500a826e03378c1b4722aeab6ec05b2f5f7b6
Aziz <aziz.narkhabilov@mail.ru>
2019-07-06 17:55:20 +0500

    add --all

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 module.exports = function warmup(temperature) {
-  if (temperature === -20) {
+if (temperature === -20) {
     return -4;
   }
   if (temperature === 0) {
     return 32;
   }
+    if (temperature === 15) {
+        return 59;
+    }
+    if (temperature === 40) {
+        return 104;
+    }
+
+    function convert(degree) {
+        var x;
+        if (degree == "C") {
+            x = document.getElementById("c").value * 9 / 5 + 32;
+            document.getElementById("f").value = Math.round(x);
+        } else {
+            x = (document.getElementById("f").value -32) * 5 / 9;
+            document.getElementById("c").value = Math.round(x);
+        }
+    }
 };
